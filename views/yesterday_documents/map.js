90e7d428f2b3196678d948b694470f78b9284492
venkateshreddy <kvredy87@gmail.com>
2014-08-20 17:38:29 +0530

    added created time to UI

--- views/yesterday_documents/map.js
+++ views/yesterday_documents/map.js
@@ -6,12 +6,18 @@ function(doc) {
 	                return (month < 10 ? '0' : '') + month
 	        }
 	 }
+	 function getTimeInFormat(timestr){
+	 	//alert(timestr);
+	 	return timestr.substring(0,4)+"-"+timestr.substring(4,6)+"-"+timestr.substring(6,8)+" "+timestr.substring(8,10)+":"+timestr.substring(10,12)+":"+timestr.substring(12,14);
+	 }
 	 var ct = doc.created_time;
 	 if(typeof ct === 'undefined')
 	 	return;
 	 if(parseInt(ct) >= parseInt(getYesterdayDateString())){
+	 	ct = getTimeInFormat(ct);
 		 emit(doc._id, {
 		      _id : doc._id,
+		      time : ct,
 		      url : doc.url,
 		      type : doc.request_type,
 		      method : doc.method_name,
